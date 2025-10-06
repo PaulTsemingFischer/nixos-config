ed9ba7e4b5f7ae21f9b1ed0b2f7e8b8d4a5ccf16
Paul Fischer <paultsemingfischer@gmail.com>
2025-10-06 10:08:23 -0400

    Cleaning up aliases

--- home-manager/aliases.nix
+++ home-manager/aliases.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   programs.zsh = {
-    #enable = true;
     shellAliases = {
       #OMZ
       ls = "exa --icons";
@@ -16,14 +15,12 @@
 
       #Edit
       ec = "sudo code ~/Documents/nix-config/nixos/configuration.nix";
+      ecd = "code ~/Documents/nix-config";
       eh = "code ~/Documents/nix-config/home-manager/home.nix";
       ea = "code ~/Documents/nix-config/home-manager/aliases.nix";
 
       #nixos
       nfmt = "nix fmt ./";
-
-      # rb = "nix fmt ~/Documents/nix-config; sudo nixos-rebuild switch --flake ~/Documents/nix-config#nixos";
-      # rh = "nix fmt ~/Documents/nix-config; home-manager switch --flake ~/Documents/nix-config#pengl@nixos";
       rb = "(cd ~/Documents/nix-config && nix fmt ./); sudo nixos-rebuild switch --flake ~/Documents/nix-config#nixos";
       rh = "(cd ~/Documents/nix-config && nix fmt ./); home-manager switch --flake ~/Documents/nix-config#pengl@nixos";
 
@@ -36,10 +33,6 @@
       gl = "git log --oneline";
       gld = "git log --oneline --decorate --graph --all";
       gs = "git status";
-      #      ga = "git add .";
-      #     gc = "git commit -m $1";
-      #    gac = "git add .; git commit -m $1";
-      #  gacp = "git add .; git commit -m $1; git push";
     };
     initExtra = ''
       #      gaa() { git add .; }
