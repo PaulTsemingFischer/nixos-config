f64eded8f2f70847ea456fb0ca1e69d3a51eda8f
Paul Fischer <paultsemingfischer@gmail.com>
2025-10-06 03:24:11 -0400

    Adding directory aliases

--- home-manager/aliases.nix
+++ home-manager/aliases.nix
@@ -10,9 +10,24 @@
       #OMZ
       ls = "exa --icons";
 
+      #Jump
+      jc = "cd ~/Documents/nix-config";
+      jh = "cd ~/Documents/nix-config/home-manager";
+
+      #Edit
+      ec = "sudo nano ~/Documents/nix-config/nixos/configuration.nix";
+      eh = "nano ~/Documents/nix-config/home-manager/home.nix";
+      ea = "nano ~/Documents/nix-config/home-manager/aliases.nix";
+
+      #nixos
       nfmt = "nix fmt ./";
-      rb = "nix fmt ./; sudo nixos-rebuild switch --flake .#nixos";
-      rh = "nix fmt ./; home-manager switch --flake .#pengl@nixos";
+
+      # rb = "nix fmt ~/Documents/nix-config; sudo nixos-rebuild switch --flake ~/Documents/nix-config#nixos";
+      # rh = "nix fmt ~/Documents/nix-config; home-manager switch --flake ~/Documents/nix-config#pengl@nixos";
+      rb = "(cd ~/Documents/nix-config && nix fmt ./); sudo nixos-rebuild switch --flake ~/Documents/nix-config#nixos";
+      rh = "(cd ~/Documents/nix-config && nix fmt ./); home-manager switch --flake ~/Documents/nix-config#pengl@nixos";
+
+      #system
       hiberate = "sudo systemctl hibernate";
       bye = "shutdown -r now";
       hg = "history | grep $1";
@@ -21,11 +36,16 @@
       gl = "git log --oneline";
       gld = "git log --oneline --decorate --graph --all";
       gs = "git status";
-      ga = "git add .";
-      gc = "git commit -m $1";
-      gac = "ga; gc;";
-      gp = "git push";
-      gacp = "ga; gc; gp;";
+      #      ga = "git add .";
+      #     gc = "git commit -m $1";
+      #    gac = "git add .; git commit -m $1";
+      #  gacp = "git add .; git commit -m $1; git push";
     };
+    initExtra = ''
+      #      gaa() { git add .; }
+      #      gcmsg() { git commit -m "$1"; }
+            gac() { gaa && gcmsg "$1"; }
+            gacp() { gaa && gcmsg "$1" && gp; }
+    '';
   };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,10 +140,10 @@
     #media-session.enable = true;
   };
 
-hardware.bluetooth = {
-enable = true;
-powerOnBoot = true;
-};
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 
   # Install firefox.
   programs.firefox.enable = true;
@@ -180,7 +180,7 @@ powerOnBoot = true;
     };
   };
 
-programs.zsh.enable = true;
+  programs.zsh.enable = true;
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
