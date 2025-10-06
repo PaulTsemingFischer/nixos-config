67ca40dbe83d1d08d5119134d2f2945506612bf4
Paul Fischer <paultsemingfischer@gmail.com>
2025-10-06 12:13:40 -0400

    prompt line customization

--- home-manager/powerline-prompt.sh
+++ home-manager/powerline-prompt.sh
@@ -1,19 +1,61 @@
-# ===== Zsh Powerline Prompt =====
+# ===== Zsh Powerline Prompt with Color Constants =====
 
-# ========== COLOR THEME ==========
-# Customize your prompt colors here
-THEME_USER_BG="151"           # pastel green - username background
-THEME_USER_FG="0"             # black - username text
-THEME_SPECIAL_DIR_BG="67"     # dark blue - special dirs (~ or custom) background
-THEME_SPECIAL_DIR_FG="255"    # white - special dirs text
-THEME_PATH_BG="74"            # blue - path background
-THEME_PATH_FG="255"           # white - path text
-THEME_GIT_BG="116"            # cyan - git branch background
-THEME_GIT_FG="0"              # black - git branch text
-THEME_PROMPT_BG="181"         # red/pink - prompt symbol background
-THEME_PROMPT_FG="255"         # white - prompt symbol text
+# ================= BASIC COLORS =================
+COLOR_BLACK=0
+COLOR_RED=196
+COLOR_GREEN=46
+COLOR_YELLOW=226
+COLOR_BLUE=21
+COLOR_MAGENTA=201
+COLOR_CYAN=51
+COLOR_WHITE=15
 
-# ========== INTERNAL COLOR VARIABLES (Do not edit below this line) ==========
+# ================= BRIGHT / LIGHT COLORS =================
+COLOR_LIGHT_GRAY=250
+COLOR_DARK_GRAY=235
+COLOR_LIGHT_BLUE=74
+COLOR_DARK_BLUE=67
+COLOR_LIGHT_RED=181
+COLOR_CYAN_LIGHT=116
+COLOR_ORANGE=208
+COLOR_PINK=212
+
+# ================= PASTEL COLORS =================
+COLOR_PASTEL_GREEN=151
+COLOR_PASTEL_GREEN_LIGHT=157
+COLOR_PASTEL_PINK=218
+COLOR_PASTEL_ORANGE=215
+COLOR_PASTEL_YELLOW=229
+COLOR_PASTEL_CYAN=159
+COLOR_PASTEL_BLUE=153
+COLOR_PASTEL_PURPLE=183
+COLOR_PASTEL_LAVENDER=225
+COLOR_PASTEL_CORAL=210
+COLOR_PASTEL_MINT=195
+
+# ================= OPTIONAL ADDITIONAL SHADES =================
+COLOR_BROWN=94
+COLOR_TAN=180
+COLOR_SOFT_BLUE=111
+COLOR_SOFT_PURPLE=147
+COLOR_SOFT_PINK=218   # same as pastel pink
+COLOR_SOFT_GREEN=120
+
+
+# ================= THEME COLORS =================
+# Assign theme elements using color constants
+THEME_USER_BG=$COLOR_PASTEL_GREEN
+THEME_USER_FG=$COLOR_BLACK
+THEME_SPECIAL_DIR_BG=$COLOR_DARK_BLUE
+THEME_SPECIAL_DIR_FG=$COLOR_WHITE
+THEME_PATH_BG=$COLOR_LIGHT_BLUE
+THEME_PATH_FG=$COLOR_WHITE
+THEME_GIT_BG=$COLOR_CYAN_LIGHT
+THEME_GIT_FG=$COLOR_BLACK
+THEME_PROMPT_BG=$COLOR_LIGHT_RED
+THEME_PROMPT_FG=$COLOR_WHITE
+
+# ================= INTERNAL COLOR VARIABLES =================
 BG_USER="%{\e[48;5;${THEME_USER_BG}m%}"
 FG_USER="%{\e[38;5;${THEME_USER_BG}m%}"
 BG_SPECIAL_DIR="%{\e[48;5;${THEME_SPECIAL_DIR_BG}m%}"
@@ -25,80 +67,38 @@ FG_GIT="%{\e[38;5;${THEME_GIT_BG}m%}"
 BG_PROMPT="%{\e[48;5;${THEME_PROMPT_BG}m%}"
 FG_PROMPT="%{\e[38;5;${THEME_PROMPT_BG}m%}"
 
-# Text colors
 TXT_USER_FG="%{\e[38;5;${THEME_USER_FG}m%}"
 TXT_SPECIAL_DIR_FG="%{\e[38;5;${THEME_SPECIAL_DIR_FG}m%}"
 TXT_PATH_FG="%{\e[38;5;${THEME_PATH_FG}m%}"
 TXT_GIT_FG="%{\e[38;5;${THEME_GIT_FG}m%}"
 TXT_PROMPT_FG="%{\e[38;5;${THEME_PROMPT_FG}m%}"
 
-# Text styles
+# ================= TEXT STYLES =================
 BOLD="%{\e[1m%}"
 ITALIC="%{\e[3m%}"
 BOLD_ITALIC="%{\e[1;3m%}"
 RESET="%{\e[0m%}"
 
-# Powerline symbols
-RIGHT_ARROW=$'\ue0b0'           # 
-GIT_LOGO=$'\ue725'              # 
-WHITE_SEPARATOR=$'\ue0b1'       # 
+# ================= POWERLINE SYMBOLS =================
+RIGHT_ARROW=$'\ue0b0'
+GIT_LOGO=$'\ue725'
+WHITE_SEPARATOR=$'\ue0b1'
 
-# --- Git branch detection ---
+# ================= GIT BRANCH DETECTION =================
 git_branch() {
   if command git rev-parse --is-inside-work-tree &>/dev/null; then
     git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD
   fi
 }
 
-# --- Path formatting with color blocks ---
+# ================= PATH FORMATTING =================
 format_path() {
   local pwd_path=$(pwd | sed "s|^$HOME|~|")
   local path_display=""
   local has_subdirs=false
   local is_special_dir=false
+  local first=
 
-  # WSL-specific path handling (commented out for Linux)
-  # if [[ $pwd_path == "/mnt/c/Users/Skylar"* ]]; then
-  #   is_special_dir=true
-  #   if [[ $pwd_path == "/mnt/c/Users/Skylar/Downloads/Code"* ]]; then
-  #     if [[ $pwd_path == "/mnt/c/Users/Skylar/Downloads/Code" ]]; then
-  #       path_display="${BG_SPECIAL_DIR}${TXT_SPECIAL_DIR_FG}${BOLD_ITALIC} code ${RESET}"
-  #       has_subdirs=false
-  #     else
-  #       local subdir=${pwd_path#/mnt/c/Users/Skylar/Downloads/Code}
-  #       path_display="${BG_SPECIAL_DIR}${TXT_SPECIAL_DIR_FG}${BOLD_ITALIC} code ${RESET}"
-  #       has_subdirs=true
-  #       local IFS='/'
-  #       for dir in $subdir; do
-  #         [[ -z $dir ]] && continue
-  #         if [[ -z $first ]]; then
-  #           path_display+="${FG_SPECIAL_DIR}${BG_PATH}${RIGHT_ARROW}${RESET}${BG_PATH}${TXT_PATH_FG} $dir"
-  #           first=1
-  #         else
-  #           path_display+=" ${TXT_PATH_FG}${WHITE_SEPARATOR}${RESET}${BG_PATH} $dir"
-  #         fi
-  #       done
-  #     fi
-  #   else
-  #     if [[ $pwd_path == "/mnt/c/Users/Skylar" ]]; then
-  #       path_display="${BG_SPECIAL_DIR}${TXT_SPECIAL_DIR_FG}${BOLD_ITALIC} winhome ${RESET}"
-  #     else
-  #       local subdir=${pwd_path#/mnt/c/Users/Skylar}
-  #       path_display="${BG_SPECIAL_DIR}${TXT_SPECIAL_DIR_FG}${BOLD_ITALIC} winhome ${RESET}"
-  #       has_subdirs=true
-  #       local IFS='/'
-  #       for dir in $subdir; do
-  #         [[ -z $dir ]] && continue
-  #         if [[ -z $first ]]; then
-  #           path_display+="${FG_SPECIAL_DIR}${BG_PATH}${RIGHT_ARROW}${RESET}${BG_PATH}${TXT_PATH_FG} $dir"
-  #           first=1
-  #         else
-  #           path_display+=" ${TXT_PATH_FG}${WHITE_SEPARATOR}${RESET}${BG_PATH} $dir"
-  #         fi
-  #       done
-  #     fi
-  #   fi
-  # el
   if [[ $pwd_path == "~"* ]]; then
     is_special_dir=true
     if [[ $pwd_path == "~" ]]; then
@@ -138,7 +138,7 @@ format_path() {
   echo "$is_special_dir"
 }
 
-# --- Prompt segments ---
+# ================= PROMPT SEGMENTS =================
 segment_user() {
   local path_info=($(format_path))
   local is_special_dir=${path_info[-1]}
@@ -186,10 +186,10 @@ segment_prompt() {
   echo " "
 }
 
-# --- Assemble final prompt ---
+# ================= ASSEMBLE FINAL PROMPT =================
 setopt prompt_subst
 PROMPT='$(segment_user)$(segment_path_and_git)$(segment_prompt)'
 
-# Optional environment
+# ================= OPTIONAL ENVIRONMENT =================
 export PATH="$HOME/bin:$PATH"
 # export VISUAL=nvim
